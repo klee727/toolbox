bcc6a3dfcf19093420b516e4a79e90d9eaad788a
klee727 <klee727@outlook.com>
2023-05-13 12:13:48 +0700

    +debug

--- netlify/functions/trader.js
+++ netlify/functions/trader.js
@@ -11,6 +11,10 @@
 
 
 exports.handler = async function (event, context) {
+
+    console.log("event", JSON.stringify(event, null, 2));
+    console.log("context", JSON.stringify(context, null, 2));
+
     // your server-side functionality
     return {
         statusCode: 200,
